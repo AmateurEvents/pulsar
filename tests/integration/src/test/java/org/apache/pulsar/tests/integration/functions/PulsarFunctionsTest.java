dad81ed90d07ea1e2ba22ac7b537ef7efdfed29d
guangning <guangning@apache.org>
2020-01-20 13:10:10 +0800

    Add find command

--- tests/integration/src/test/java/org/apache/pulsar/tests/integration/functions/PulsarFunctionsTest.java
+++ tests/integration/src/test/java/org/apache/pulsar/tests/integration/functions/PulsarFunctionsTest.java
@@ -1002,7 +1002,7 @@ public abstract class PulsarFunctionsTest extends PulsarFunctionsTestBase {
         ContainerExecResult containerExecResult = pulsarCluster.getAnyWorker().execCmd(commands);
         assertTrue(containerExecResult.getStdout().contains("\"Created successfully\""));
 
-        pulsarCluster.getAnyWorker().execCmdAsync("find", "/tmp/functions", "-name", "*.log");
+        pulsarCluster.getAnyWorker().execCmdAsync("find", "/tmp", "-name", "*.log");
 
         // /tmp/functions/public/default/test-sliding-window-fn-shstbkai
         pulsarCluster.getAnyWorker().execCmdAsync("tail", "-f", "/tmp/functions/public/default/" + functionName + "/" + functionName + "-0" + ".log");
